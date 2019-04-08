6b4044d8c21f0d4e88bb1f8965240fc0ea5dad89
赵振智 <zhaozhenzhi@xinyunlian.com>
2019-04-08 22:05:20 +0800

    1

---
+++ all/src/main/java/cn/dazhiyy/high/App.java
@@ -0,0 +1,20 @@
+package cn.dazhiyy.high;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author dazhi
+ * @projectName high
+ * @packageName cn.dazhiyy.high
+ * @className App
+ * @description TODO
+ * @date 2019/4/8 22:01
+ */
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class,args);
+    }
+}
